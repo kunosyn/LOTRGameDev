44193ecbbab05ae6332747a55a787c5d1f8692bb
kunosyn <kunosyn@gmail.com>
2024-06-29 01:37:59 -0500

    fix: change file extension to luau
    Change file extension to luau.

--- src/client/Controllers/SkillsController/Skills.lua
+++
@@ -1,9 +0,0 @@
-local Skills = { }
-
-Skills['0:4'] = function(actionName: string, inputState: Enum.UserInputState, input: InputObject)
-    if inputState ~= Enum.UserInputState.Begin then return end
-    
-    print('triggered')
-end
-
-return Skills
